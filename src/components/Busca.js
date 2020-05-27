9c721b7cb553e61404bd80a56cb22f736b186bbc
Harley Macedo <harley.ip@gmail.com>
2020-05-27 08:44:28 -0300

    Primeiro commit

---
+++ src/components/Busca.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Busca extends Component {
+    render() {
+        return(
+            <div>
+                <p>Teste de Componente</p>
+            </div>
+        )
+    }
+}
