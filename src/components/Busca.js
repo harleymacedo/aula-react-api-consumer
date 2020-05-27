b9f3b12cd9aaa8daec5e8934c7baa4b3f87a7e4a
Harley Macedo <harley.ip@gmail.com>
2020-05-27 10:11:27 -0300

    Componente criado

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,10 +1,50 @@
 import React, {Component} from 'react'
 
 export default class Busca extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            textoEntrada: '',
+            textoResultado: '',
+            textoArea: '',
+        }
+        this.buscar = this.buscar.bind(this)
+        this.atualizarTexto = this.atualizarTexto.bind(this)
+    }
+
+    atualizarTexto(event) {
+        console.log(event.target.value)
+        this.setState({textoEntrada: event.target.value})
+      }
+    
+    buscar() {
+    let url = 'https://aula-node-api-server.herokuapp.com/api/' + this.state.textoEntrada
+    console.log(url)
+    fetch(url)
+    .then( (resultado) => {
+        return resultado.json()
+    })
+    .then( (resultado) => {
+        let textoResult = 'Nome: ' + resultado.nome
+        let textoAreaResult = 'Área: ' + resultado.area
+        console.log(textoResult)
+        this.setState({textoResultado: textoResult, textoArea: textoAreaResult})
+    })
+    .catch( (error) => {
+        this.setState({textoResultado: 'Valor não encontrado'})
+    })
+    }
+
     render() {
         return(
             <div>
-                <p>Teste de Componente</p>
+                <h2>Busca rápida de Profs</h2>
+                <h3>Informe o código:</h3>
+                <input type='text' placeholder='0 - 3' onChange={this.atualizarTexto} />
+                <input type='button' onClick={this.buscar} value='Buscar' />
+                <h4>{this.state.textoResultado}</h4>
+                <h4>{this.state.textoArea}</h4>
             </div>
         )
     }
